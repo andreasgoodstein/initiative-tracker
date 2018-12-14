029a0c30e09645a96e97ce7b1e3a8965a2eaac15
Andreas Goodstein <andreasgoodstein@gmail.com>
2018-12-14 14:36:31 +0100

    Update tests

--- src/components/display/actor/Actor.spec.tsx
+++ src/components/display/actor/Actor.spec.tsx
@@ -6,8 +6,12 @@ import IActor from 'entities/IActor';
 import Actor from './index';
 
 const testActor: IActor = {
+    clone: () => ({} as IActor),
+    hasActiveTurn: false,
+    id: 1,
     initiativeBonus: 30,
     name: 'Test Actor',
+    sort: () => 0,
 };
 
 describe('the Actor component', () => {
--- src/components/display/actorList/ActorList.spec.tsx
+++ src/components/display/actorList/ActorList.spec.tsx
@@ -7,14 +7,18 @@ import ActorList from './index';
 
 const testList: IActor[] = [
     {
+        clone: () => ({} as IActor),
+        hasActiveTurn: false,
+        id: 1,
         initiativeBonus: 30,
         name: 'Test Actor',
+        sort: () => 0,
     },
 ];
 
 describe('the ActorList component', () => {
     it('renders', () => {
-        const wrapper = shallow(<ActorList items={[...testList]}/>);
+        const wrapper = shallow(<ActorList items={[...testList]} removeActor={() => undefined}/>);
 
         expect(wrapper).not.toBeNull();
         expect(wrapper).toMatchSnapshot();
