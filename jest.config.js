a2aeda8389d13100a253d40c5447b7667e58fed1
Andreas Goodstein <andreasgoodstein@gmail.com>
2019-09-03 23:05:46 +0200

    update tech stack

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,19 @@
-const { defaults: tsJestConfig } = require('ts-jest/presets');
+const { defaults: tsJestConfig } = require("ts-jest/presets");
 
 module.exports = {
   ...tsJestConfig,
-  preset: 'jest-expo',
+  preset: "jest-expo",
   transform: {
     ...tsJestConfig.transform,
-    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    "\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js"
   },
   globals: {
-    'ts-jest': {
-      tsConfig: './test/tsconfig.jest.json',
-    },
+    "ts-jest": {
+      tsConfig: "./test/tsconfig.jest.json"
+    }
   },
-  modulePaths: ['<rootDir>/src'],
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/test/',
-    '/assets/'
-  ],
-  setupTestFrameworkScriptFile: '<rootDir>/test/setupEnzyme.js',
+  modulePaths: ["<rootDir>/src"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/test/", "/assets/"],
+  setupFilesAfterEnv: ["<rootDir>/test/setupEnzyme.js"],
   snapshotSerializers: ["enzyme-to-json/serializer"]
 };
