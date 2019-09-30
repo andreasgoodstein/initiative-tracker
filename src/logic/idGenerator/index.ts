01d2c58bcfad9d12953750a18822a82a4a12b489
Andreas Goodstein <andreasgoodstein@gmail.com>
2019-09-30 23:36:06 +0200

    Delete index.ts

--- src/logic/idGenerator/index.ts
+++
@@ -1,9 +0,0 @@
-// Clojure that returns a new id on each call
-export default (() => {
-    let runningId: number = 0;
-
-    return (): number => {
-        runningId += 1;
-        return runningId;
-    };
-})();
