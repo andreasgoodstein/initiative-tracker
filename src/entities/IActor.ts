4b033795844d03c5350e52b7364a9b2aebe38f43
Andreas Goodstein <andreasgoodstein@gmail.com>
2019-09-30 23:35:25 +0200

    Update IActor.ts

--- src/entities/IActor.ts
+++ src/entities/IActor.ts
@@ -2,12 +2,8 @@ export default interface IActor {
   readonly id: number;
   readonly name: string;
 
-  initiative?: number;
-
   hasActiveTurn: boolean;
-
-  clone(id: number | undefined): IActor;
-  sort(a: IActor, b: IActor): number;
+  initiative: number;
 }
 
 export class Actor implements IActor {
@@ -15,21 +11,16 @@ export class Actor implements IActor {
   public readonly name: string;
 
   public hasActiveTurn: boolean;
-
-  public initiative?: number;
+  public initiative: number;
 
   constructor(id: number, name: string) {
     this.id = id;
     this.name = name;
 
     this.hasActiveTurn = false;
-  }
-
-  public clone(id: number | undefined): Actor {
-    return new Actor(id || this.id, this.name);
-  }
-
-  public sort(a: Actor, b: Actor): number {
-    return (b.initiative || 0) - (a.initiative || 0);
+    this.initiative = 0;
   }
 }
+
+export const sortActors = (firstActor: Actor, secondActor: Actor): number =>
+  secondActor.initiative - firstActor.initiative;
